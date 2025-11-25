68774fa9928073f54f2e78a428ad5c59d99fabb9
Akash Sharma <akashpics1994@gmail.com>
2025-11-25 17:49:15 +0530

    upaded

--- copyfile.groovy
+++ copyfile.groovy
@@ -2,17 +2,26 @@ pipeline {
     agent { label 'LAB-windows-EV' }
 
     parameters {
-        choice(name: 'SERVER', choices: ['PPG-App01', 'PPG-App02'])
+        choice(
+            name: 'SERVER',
+            choices: ['PPG-App01', 'PPG-App02'],
+            description: 'Select remote Windows server'
+        )
     }
 
     stages {
         stage('Run PowerShell Script') {
             steps {
                 powershell """
-                Invoke-Command -ComputerName ${params.SERVER} -ScriptBlock {
-                    # Run your PS1 file
-                     D:\\Jenkins\\Agent\\workspace\\windowsPSwith-git\\day1.ps1
-                }
+                # Optional: If using credentials stored in Jenkins
+                # \$cred = Get-Credential
+
+                Invoke-Command `
+                    -ComputerName ${params.SERVER} `
+                    -ScriptBlock {
+                        Write-Host "Running script on remote server: ${params.SERVER}"
+                        & "D:\\\\Jenkins\\\\Agent\\\\workspace\\\\windowsPSwith-git\\\\day1.ps1"
+                    }
                 """
             }
         }
